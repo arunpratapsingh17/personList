6bb14587a9fac6d306069038bfe69a682e24df69
arunpratapsingh17 <218apsj@gmail.com>
2020-01-04 18:49:07 +0530

    First Commit on personList

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+const Person = ({ name, img, job, children }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (
+    <section className="person">
+      <img style={{ width: "100px" }} src={url}></img>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+    </section>
+  );
+};
+const PersonList = () => {
+  return (
+    <section className="personList">
+      <Person name="Alpha" job="developer" img="34" />
+      <Person name="Beta" job="Doctor" img="69" />
+      <Person name="Gamma" job="Professor" img="61" />
+    </section>
+  );
+};
+ReactDom.render(<PersonList></PersonList>, document.getElementById("root"));
